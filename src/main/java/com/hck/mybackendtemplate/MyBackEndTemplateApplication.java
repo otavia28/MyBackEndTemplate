5cbf6d768acc4c0e9b43aa5c595edb4d927ffd78
otavia28 <huchenkun28@163.com>
2024-04-24 17:34:49 +0800

    Initial commit

---
+++ src/main/java/com/hck/mybackendtemplate/MyBackEndTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.hck.mybackendtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBackEndTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBackEndTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hck/mybackendtemplate/MyBackEndTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hck.mybackendtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBackEndTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
