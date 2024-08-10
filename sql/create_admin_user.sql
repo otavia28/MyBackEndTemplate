ef8a330872641ccce9dae1a2ec2e58daada004d4
otavia28 <huchenkun28@163.com>
2024-08-10 17:14:03 +0800

    Update create_admin_user.sql

--- sql/create_admin_user.sql
+++ sql/create_admin_user.sql
@@ -1,4 +1,7 @@
-use 数据库名;
 INSERT INTO user
-    (user_id, user_name, user_account, user_avatar, gender, user_password, phone, email, user_status, create_time, update_time, isDelete, user_role)
-    VALUES (1, '小明', 'xiaoming', 'https://th.bing.com/th?id=OIP.54qlbLNAZ64K94c_DCT-qAAAAA&w=166&h=166&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2', 1, '962cfd372de03a508c957a86aa991f25', '19999999999', '19999999999@fake163.com', 0, '2024-04-19 09:43:00', '2024-04-19 09:44:11', 0, 1);
+(user_id, user_name, user_account, user_avatar, gender, user_password, phone, email, user_status, create_time,
+ update_time, isDelete, user_role)
+VALUES (1, '小明', 'xiaoming',
+        'https://th.bing.com/th?id=OIP.54qlbLNAZ64K94c_DCT-qAAAAA&w=166&h=166&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2',
+        1, '962cfd372de03a508c957a86aa991f25', '19999999999', '19999999999@fake163.com', 0, '2024-04-19 09:43:00',
+        '2024-04-19 09:44:11', 0, 1);
